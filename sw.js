805b2d2af7a7ad5fbd6eda8c38f826b1ff55f52b
tuhin <tuhinraja433@gmail.com>
2025-12-26 16:53:41 +0600

    spelling misstake replace

--- sw.js
+++ sw.js
@@ -1,38 +1,52 @@
-const CACHE_NAME = "noteeve-v2";
+const CACHE_NAME = "noteeve-v5";
 
-const urlsToCache = [
-  "/NoteEve/",
-  "/NoteEve/index.html",
-  "/NoteEve/manifest.json",
-  "/NoteEve/css/style.css",
-  "/NoteEve/js/app.js",
-  "/NoteEve/img/icon-192.png",
-  "/NoteEve/img/icon-512.png",
-  "/NoteEve/fontawesome-free-7.0.0-web/css/all.min.css",
+const APP_ROOT = "/NoteEve/";
+
+const ASSETS = [
+  APP_ROOT,
+  APP_ROOT + "index.html",
+  APP_ROOT + "manifest.json",
+
+  // CSS & JS
+  APP_ROOT + "css/style.css",
+  APP_ROOT + "js/app.js",
+
+  // Font Awesome
+  APP_ROOT + "fontawesome-free-7.0.0-web/css/all.min.css",
+  APP_ROOT + "fontawesome-free-7.0.0-web/webfonts/fa-solid-900.woff2",
+  APP_ROOT + "fontawesome-free-7.0.0-web/webfonts/fa-regular-400.woff2",
+  APP_ROOT + "fontawesome-free-7.0.0-web/webfonts/fa-brands-400.woff2",
+
+  // Icons
+  APP_ROOT + "img/icon-192.png",
+  APP_ROOT + "img/icon-512.png"
 ];
 
+// INSTALL
 self.addEventListener("install", event => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
   );
   self.skipWaiting();
 });
 
+// ACTIVATE
 self.addEventListener("activate", event => {
   event.waitUntil(self.clients.claim());
 });
 
+// FETCH (🔥 refresh + offline safe)
 self.addEventListener("fetch", event => {
   if (event.request.mode === "navigate") {
     event.respondWith(
-      caches.match("/NoteEve/index.html")
+      caches.match(APP_ROOT + "index.html")
     );
     return;
   }
 
   event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
+    caches.match(event.request).then(res => {
+      return res || fetch(event.request);
     })
   );
 });
