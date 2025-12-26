90e7574b0953579de0857989c3b970a5b45ce27e
tuhin <tuhinraja433@gmail.com>
2025-12-26 15:39:06 +0600

    add

--- sw.js
+++ sw.js
@@ -1,11 +1,23 @@
-const CACHE_NAME = "noteeve-v2";
+const CACHE_NAME = "noteeve-v3";
 
 const urlsToCache = [
   "/NoteEve/",
   "/NoteEve/index.html",
   "/NoteEve/manifest.json",
+
+  // CSS & JS
   "/NoteEve/css/style.css",
   "/NoteEve/js/app.js",
+
+  // Font Awesome CSS
+  "/NoteEve/fontawesome-free-7.0.0-web/css/all.min.css",
+
+  // Font Awesome Fonts (most common)
+  "/NoteEve/fontawesome-free-7.0.0-web/webfonts/fa-solid-900.woff2",
+  "/NoteEve/fontawesome-free-7.0.0-web/webfonts/fa-regular-400.woff2",
+  "/NoteEve/fontawesome-free-7.0.0-web/webfonts/fa-brands-400.woff2",
+
+  // Icons
   "/NoteEve/img/icon-192.png",
   "/NoteEve/img/icon-512.png"
 ];
