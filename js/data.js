3db0099d7311b2d39b6462757c909deb2e62c014
tuhin <tuhinraja433@gmail.com>
2025-12-29 15:11:17 +0600

    added code

--- js/data.js
+++ js/data.js
@@ -6,13 +6,8 @@ export const audiosData = {
             audioSource: "audios/",
         },
         {
-            audio: "Relaxing_Zen_Music_with_Water_Sounds_•_Peaceful_Ambience_for_Spa.m4a",
-            song_name: "sound 2",
-            audioSource: "audios/",
-        },
-        {
             audio: "Soothing_Relaxation_Relaxing_Piano_Music_Water_Sounds_for_Sleep.m4a",
-            song_name: "sound 3",
+            song_name: "sound 2",
             audioSource: "audios/",
         }
     ]
