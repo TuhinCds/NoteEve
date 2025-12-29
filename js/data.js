47680bc2d5b0cc7a0938e3affdd0a83a6c15bcad
tuhin <tuhinraja433@gmail.com>
2025-12-29 15:47:48 +0600

    add

--- js/data.js
+++ js/data.js
@@ -1,11 +1,6 @@
 export const audiosData = {
     audios: [
         {
-            audio: "Sunny_Mornings_Beautiful_Relaxing_Music_with_Piano_Guitar_Bird_Sounds.m4a",
-            song_name: "sound 1",
-            audioSource: "audios/",
-        },
-        {
             audio: "Soothing_Relaxation_Relaxing_Piano_Music_Water_Sounds_for_Sleep.m4a",
             song_name: "sound 2",
             audioSource: "audios/",
