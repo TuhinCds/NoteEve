e895573af25b2c404b7a1c55a7d3994d71435b83
tuhin <tuhinraja433@gmail.com>
2026-01-05 19:18:48 +0600

    fixed bug

--- js/app.js
+++ js/app.js
@@ -1527,7 +1527,7 @@ function fullScreenOverlyRemove() {
 function fullScreenOverlyClick() {
     fullScreenOverlyRemove()
     removeAlert()
-    LinkMenu()
+    RemoveLinkMenu()
 }   
 
 fullScreenOverly.addEventListener("click", () => {
